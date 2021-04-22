040958c74dd01d5fa5a205cced563aa57c958892
Giuseppe Lopes <giuseppe.lopes@gmail.com>
2021-04-22 13:44:42 -0300

    Versao com Aula 1 concluida

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Welcome } from './src/pages/Welcome';
+
+export default function App() {
+  return (
+    <Welcome />
+  );
+}
+
+// Codigo desafio aula 1 = Missao Espacial #missaoespacial
+// Codigo desafio aula 2 = 
+// Codigo desafio aula 3 = 
